d6a41b71da9e8bbc8af97059f1ba0786189a669a
2cong <cjina0418@gmail.com>
2020-08-01 15:48:13 +0900

    add: 음식점 Create API 생성

--- restaurant/store/urls.py
+++ restaurant/store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DetailView
+from .views import DetailView, CreateStoreView
 
 urlpatterns = [
     path('/detail',DetailView.as_view()),
-        ]
+    path('',CreateStoreView.as_view())
+    ]
--- restaurant/store/views.py
+++ restaurant/store/views.py
@@ -2,6 +2,8 @@ import json
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
+from django.db import transaction
+
 from .models import Menu, Store
 
 # 음식점 Detail API
@@ -32,3 +34,37 @@ class DetailView(View):
 
         except KeyError:
             return JsonResponse({"message":"DATA ERROR"}, status = 400)
+
+# 음식점 Create API
+class CreateStoreView(View):
+    @transaction.atomic
+    def post(self, request):
+        try:
+            restaurant_data = json.loads(request.body)
+
+            # request body를 통해 받은 값을 db에 추가
+            Store.objects.create(
+                name = restaurant_data['name'],
+                description = restaurant_data['description'],
+                address = restaurant_data['address'],
+                phone_number = restaurant_data['phone_number']
+            )
+
+            # db에 추가한 값 중 마지막 id의 정보 불러오기
+            new_store = Store.objects.latest('id')
+
+            # 추가된 음식점 정보 불러오기
+            restaurant_detail = {
+                "id" : new_store.id,
+                "name" : new_store.name,
+                "description" : new_store.description,
+                "address" : new_store.address,
+                "phone_number" : new_store.phone_number,
+            }
+
+            return JsonResponse({"data":restaurant_detail}, status = 201)
+
+        except KeyError:
+            return JsonResponse({"message":"DATA ERROR"}, status = 400)
+        except Exception as e:
+            return JsonResponse({"message":f"{e}"}, status = 500)
