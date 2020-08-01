dfd6b41a6309f8c77d6520240a6bd29872aa30c9
2cong <cjina0418@gmail.com>
2020-08-01 16:41:11 +0900

    add: 메뉴 리스트 api 생성

--- restaurant/store/urls.py
+++ restaurant/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DetailView, CreateStoreView, CreateMenuView
+from .views import ListView, DetailView, CreateStoreView, CreateMenuView
 
 urlpatterns = [
+    path('/list', ListView.as_view()),
     path('/detail', DetailView.as_view()),
     path('', CreateStoreView.as_view()),
     path('/menu', CreateMenuView.as_view())
