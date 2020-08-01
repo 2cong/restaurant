4a5d4ae54a30a1e843c4702c54e92f1eb8824c6e
2cong <cjina0418@gmail.com>
2020-08-02 02:01:55 +0900

    add: store_list

--- restaurant_drf/store/views.py
+++ restaurant_drf/store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Store, Menu
+from .serializers import StoreSerializer, MenuSerializer
+
+
+class StoreListView(ListCreateAPIView):
+    def get(self, request):
+        restaurants = Store.objects.all()
+        serializer = StoreSerializer(restaurants, many=True)
+
+        return Response(serializer.data)
